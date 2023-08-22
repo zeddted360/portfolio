0af527d6f703bacad599bebea09e892e63975141
zeddted360 <nwiboazubuike@gmail.com>
2023-08-21 19:19:28 -0700

    s

--- server/routes.js
+++ server/routes.js
@@ -19,25 +19,7 @@ router.post('/message',async (req,res)=> {
                 message,
             }); 
        
-            const mailOptions = {
-              from: 'nwiboazubuike@gmail.com',  
-              to: email,            
-              subject: 'Message from zedd',
-              text: 'Thanks for reaching zedd,\n you\'ll be replied shortly?',
-            };
-  // Create email data
-            
-            try {
-              // Send email
-              await transporter.sendMail(mailOptions);
-              console.log('Email sent successfully');
-              res.status(200).send('Email sent successfully');
-            } 
-            catch (err) {
-              console.error(`Error sending email: ${err.message}`);
-              res.status(500).send('Error sending email');
-            }
-    res.json(message);
+    res.json({message:newMassage});
     }
     catch(err){
         console.log(err.message)
@@ -46,6 +28,23 @@ router.post('/message',async (req,res)=> {
     }
 
     
- 
+  // Create email data
+  const mailOptions = {
+    from: 'nwiboazubuike@gmail.com',  
+    to: email,            
+    subject: 'Message from zedd',
+    text: 'Thanks for reaching zedd,\n you\'ll be replied shortly?',
+  };
+  
+  try {
+    // Send email
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+    // res.status(200).send('Email sent successfully');
+  } 
+  catch (err) {
+    console.error(`Error sending email: ${err.message}`);
+    // res.status(500).send('Error sending email');
+  }
 });
 module.exports = router;
